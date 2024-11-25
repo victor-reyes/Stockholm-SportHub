06ffd53c0c871ea1686c7dd1b611f78fa002375e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-25 18:40:02 +0100

    Style /book Form with shadcn/ui Card components

--- src/app/boka/page.tsx
+++ src/app/boka/page.tsx
@@ -1,3 +1,10 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Form from "next/form";
 
@@ -5,36 +12,45 @@ const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
 export default function Book() {
   return (
-    <>
-      <div className="max-w-xs m-auto">
-        <h1>Search Available slots</h1>
-        <Form action="" className="flex flex-col gap-4">
-          <Input
-            type="search"
-            name="place"
-            placeholder="What place do you want to book?"
-          />
-          <Input
-            type="search"
-            name="sport"
-            placeholder="What activity do you want to book?"
-          />
-          <Input type="date" name="date" />
-          <Input type="time" name="time" />
-          <div className="flex gap-1 text-sm">
-            {DAYS.map((day) => (
-              <label key={day} className="flex items-center gap-1">
-                <Input
-                  type="checkbox"
-                  name="weekdays"
-                  value={day.toLowerCase()}
-                />
-                {day}
-              </label>
-            ))}
-          </div>
-        </Form>
-      </div>
-    </>
+    <main className="p-4">
+      <Card className="max-w-sm m-auto">
+        <CardHeader>
+          <CardTitle>Search Available slots</CardTitle>
+          <CardDescription>
+            Find available slots for your favorite activities in your favorite
+            places.
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <Form action="" className="flex flex-col gap-4">
+            <Input
+              type="search"
+              name="place"
+              placeholder="What place do you want to book?"
+            />
+            <Input
+              type="search"
+              name="sport"
+              placeholder="What activity do you want to book?"
+            />
+            <Input type="date" name="date" />
+            <Input type="time" name="time" />
+            <div className="flex gap-1 text-sm">
+              {DAYS.map((day) => (
+                <label key={day} className="flex items-center gap-1">
+                  <Input
+                    type="checkbox"
+                    name="weekdays"
+                    value={day.toLowerCase()}
+                  />
+                  {day}
+                </label>
+              ))}
+            </div>
+          </Form>
+        </CardContent>
+      </Card>
+    </main>
   );
 }
