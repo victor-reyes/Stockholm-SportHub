a69a0eea2189d7737e4e842d2611553f446db489
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-25 17:53:45 +0100

    Use shadcn/ui Input in /book Form

--- src/app/boka/page.tsx
+++ src/app/boka/page.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@/components/ui/input";
 import Form from "next/form";
 
 const DAYS = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
@@ -8,22 +9,22 @@ export default function Book() {
       <div className="max-w-xs m-auto">
         <h1>Search Available slots</h1>
         <Form action="" className="flex flex-col gap-4">
-          <input
+          <Input
             type="search"
             name="place"
             placeholder="What place do you want to book?"
           />
-          <input
+          <Input
             type="search"
             name="sport"
             placeholder="What activity do you want to book?"
           />
-          <input type="date" name="date" />
-          <input type="time" name="time" />
+          <Input type="date" name="date" />
+          <Input type="time" name="time" />
           <div className="flex gap-1 text-sm">
             {DAYS.map((day) => (
               <label key={day} className="flex items-center gap-1">
-                <input
+                <Input
                   type="checkbox"
                   name="weekdays"
                   value={day.toLowerCase()}
