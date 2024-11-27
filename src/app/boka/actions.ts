ac4fcde516ba13f920f591acdbc37782e8538a17
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-27 23:50:04 +0100

    Get, transform and log form data

--- src/app/boka/actions.ts
+++ src/app/boka/actions.ts
@@ -1,5 +1,21 @@
 "use server";
 
+import { service } from "@/features";
+
 export async function searchAction(formData: FormData) {
-  console.log(formData.entries().toArray());
+  const allFacilities = await service.getAllFacilities();
+
+  const facilities = formData
+    .getAll("facility")
+    .map((facility) => allFacilities.find((f) => f.name === facility)!.id);
+
+  const sports = formData.getAll("sport");
+  const date = formData.get("date");
+  const time = formData.get("time");
+
+  const fromTimestamp = new Date(`${date}T${time}:00Z`);
+  fromTimestamp.setMinutes(Math.floor(fromTimestamp.getMinutes() / 15) * 15);
+
+  console.log("facilities", facilities);
+  console.log("fromTimestamp", fromTimestamp);
 }
