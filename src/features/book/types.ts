fd24f7b183dfd92ca33b1082c9b03f71a301df67
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-27 15:49:57 +0100

    Create types for all tables select/insert statements

---
+++ src/features/book/types.ts
@@ -0,0 +1,22 @@
+import {
+  bookings,
+  facilities,
+  facilitiesToSports,
+  sports,
+  users,
+} from "@/db/schema";
+
+export type UserInsert = typeof users.$inferInsert;
+export type UserSelect = typeof users.$inferSelect;
+
+export type FacilityInsert = typeof facilities.$inferInsert;
+export type FacilitySelect = typeof facilities.$inferSelect;
+
+export type SportInsert = typeof sports.$inferInsert;
+export type SportSelect = typeof sports.$inferSelect;
+
+export type FacilitiesToSportsInsert = typeof facilitiesToSports.$inferInsert;
+export type FacilitiesToSportsSelect = typeof facilitiesToSports.$inferSelect;
+
+export type BookingInsert = typeof bookings.$inferInsert;
+export type BookingSelect = typeof bookings.$inferSelect;
