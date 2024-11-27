d16f44c3a53450355c8f7f15de7df0fc22b47a6f
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-27 16:08:19 +0100

    Implement functionality to be able see bookings status per facility given chosen facilities, sports and to and from dates

--- src/features/book/repository.ts
+++ src/features/book/repository.ts
@@ -1,9 +1,35 @@
 import { DB } from "@/db";
+import { bookings, sports } from "@/db/schema";
+import { and, gte, inArray } from "drizzle-orm";
 
 export function createRepository(db: DB) {
   return {
     async getAllBookings() {
       return await db.query.bookings.findMany();
+    },
+
+    async getFalicityBookings(
+      facilityIds?: number[],
+      sportsIds?: number[],
+      fromDate?: Date,
+      toDate?: Date,
+    ) {
+      const facilityIn = facilityIds
+        ? inArray(bookings.facilityId, facilityIds)
+        : undefined;
+      const sportIn = sportsIds ? inArray(sports.id, sports) : undefined;
+      const fromDateGte = fromDate
+        ? gte(bookings.bookingDate, fromDate)
+        : undefined;
+      const toDateLte = toDate ? gte(bookings.bookingDate, toDate) : undefined;
+
+      return await db.query.facilities.findMany({
+        with: {
+          bookings: true,
+          facilitiesSports: { with: { sport: true } },
+        },
+        where: and(facilityIn, sportIn, fromDateGte, toDateLte),
+      });
     },
   };
 }
