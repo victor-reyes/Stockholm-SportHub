df324d9d522b559e5acc50538285f3b06788124a
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-27 14:49:21 +0100

    Define drizzle relations between databases

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,12 +7,17 @@ import {
   date,
   time,
 } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 25 }).notNull(),
   email: varchar("email", { length: 100 }).notNull().unique(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  bookings: many(bookings),
+}));
 
 export const bookings = pgTable("bookings", {
   id: serial("id").primaryKey(),
@@ -24,22 +29,56 @@ export const bookings = pgTable("bookings", {
   createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
 });
 
+export const bookingsRelations = relations(bookings, ({ one }) => ({
+  user: one(users, {
+    fields: [bookings.userId],
+    references: [users.id],
+  }),
+  facility: one(facilities, {
+    fields: [bookings.facilityId],
+    references: [facilities.id],
+  }),
+}));
+
 export const facilities = pgTable("facilities", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 50 }).notNull(),
   description: varchar("description", { length: 255 }),
 });
 
+export const facilitiesRelations = relations(facilities, ({ many }) => ({
+  bookings: many(bookings),
+  facilitiesSports: many(facilitiesToSports),
+}));
+
 export const sports = pgTable("sports", {
   id: serial("id").primaryKey(),
   sport: varchar("sport", { length: 10 }).$type<Sport>().notNull(),
 });
 
-export const facilitiesSports = pgTable("facilities_sports", {
+export const sportsRelations = relations(sports, ({ many }) => ({
+  facilitiesSports: many(facilitiesToSports),
+}));
+
+export const facilitiesToSports = pgTable("facilities_to_sports", {
   id: serial("id").primaryKey(),
   facilityId: integer("facility_id").references(() => facilities.id),
   sportId: integer("sport_id").references(() => sports.id),
 });
+
+export const facilitiesToSportsRelations = relations(
+  facilitiesToSports,
+  ({ one }) => ({
+    facility: one(facilities, {
+      fields: [facilitiesToSports.facilityId],
+      references: [facilities.id],
+    }),
+    sport: one(sports, {
+      fields: [facilitiesToSports.sportId],
+      references: [sports.id],
+    }),
+  }),
+);
 
 export type Sport =
   | "football"
