db5239054265dd6f17b2c00779f9888b46234ecc
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-27 13:12:51 +0100

    Add facilities, sports and facilities to sport tables together with Sport type

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,7 +7,6 @@ import {
   date,
   time,
 } from "drizzle-orm/pg-core";
-import { db } from ".";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -15,17 +14,38 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 100 }).notNull().unique(),
 });
 
-export const bookings = pgTable(
-  "bookings",
-  {
-    id: serial("id").primaryKey(),
-    userId: integer("user_id").references(() => users.id),
-    bookingDate: date("booking_date").notNull(),
-    startTime: time("start_time").notNull(),
-    endTime: time("end_time").notNull(),
-    createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
-  },
-);
+export const bookings = pgTable("bookings", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id),
+  facilityId: integer("facility_id").references(() => facilities.id),
+  bookingDate: date("booking_date").notNull(),
+  startTime: time("start_time").notNull(),
+  endTime: time("end_time").notNull(),
+  createdAt: timestamp("created_at", { mode: "date" }).defaultNow(),
+});
 
+export const facilities = pgTable("facilities", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 50 }).notNull(),
+  description: varchar("description", { length: 255 }),
+});
 
-db.select()
+export const sports = pgTable("sports", {
+  id: serial("id").primaryKey(),
+  sport: varchar("sport", { length: 10 }).$type<Sport>().notNull(),
+});
+
+export const facilitiesSports = pgTable("facilities_sports", {
+  id: serial("id").primaryKey(),
+  facilityId: integer("facility_id").references(() => facilities.id),
+  sportId: integer("sport_id").references(() => sports.id),
+});
+
+export type Sport =
+  | "football"
+  | "basketball"
+  | "volleyball"
+  | "tennis"
+  | "badminton"
+  | "ice hockey"
+  | "floorball";
