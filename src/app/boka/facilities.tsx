0548e51bc6bd600204b51cf4cd7a1724575d99ff
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2024-11-28 16:09:35 +0100

    Add show occupied Swtich component and show occupied time slots based on its state

--- src/app/boka/facilities.tsx
+++ src/app/boka/facilities.tsx
@@ -1,23 +1,42 @@
-import { service } from "@/features";
+"use client";
+
 import { TimeSlotItem } from "./time-slot-item";
+import { Switch } from "@/components/ui/switch";
+import { Label } from "@/components/ui/label";
 import { FacilitiesBookingWithTimeSlots } from "@/features/book/types";
+import { useState } from "react";
 
 type Props = {
   facilities: FacilitiesBookingWithTimeSlots[];
 };
 
 export function Facilities({ facilities }: Props) {
+  const [showOccupied, setShowOccupied] = useState(false);
+
   return (
     <div>
-      <h2>Facilities</h2>
+      <div className="flex gap-2 items-baseline justify-between">
+        <h2>Facilities</h2>
+
+        <div className="flex gap-1 items-center">
+          <Switch
+            checked={showOccupied}
+            onCheckedChange={setShowOccupied}
+            id="availability-mode"
+          ></Switch>
+          <Label htmlFor="availability-mode">Show occupied</Label>
+        </div>
+      </div>
       <ul>
         {facilities.map((facility) => (
           <li key={facility.id}>
             {facility.name}
             <ul className="not-prose space-y-2">
-              {facility.timeSlots.map((timeSlot, index) => (
-                <TimeSlotItem key={index} timeSlot={timeSlot} />
-              ))}
+              {facility.timeSlots
+                .filter((timeSlot) => showOccupied || timeSlot.state === "free")
+                .map((timeSlot, index) => (
+                  <TimeSlotItem key={index} timeSlot={timeSlot} />
+                ))}
             </ul>
           </li>
         ))}
